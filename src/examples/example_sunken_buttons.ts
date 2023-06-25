1e3cb9e353f504ce53ece304ee0479764039ed28
Edward Tanguay (windows) <edward@tanguay.info>
2023-06-25 13:06:39 +0200

    with icons

--- src/examples/example_sunken_buttons.ts
+++ src/examples/example_sunken_buttons.ts
@@ -7,15 +7,10 @@ export const example_sunken_buttons = () => {
 	let html = '';
 	html += `
 	<section class="buttons-container">
-		<button>
-			<img src="images/scissors.svg" alt="">
-			Cut</button>
-		<button>
-			<img src="images/file.svg" alt="">
-			Copy</button>
-		<button>
-			<img src="images/paste.svg" alt="">
-			Paste</button>
+		<button><i class="fa fa-user" aria-hidden="true"></i> Profile</button>
+		<button><i class="fa fa-envelope-o" aria-hidden="true"></i> Mail</button>
+		<button><i class="fa fa-video-camera" aria-hidden="true"></i>
+ Videos</button>
 	</section>
 	`;
 	return wrapAsExample('example_sunken_buttons', html, description);
