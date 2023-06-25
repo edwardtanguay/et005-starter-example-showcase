b74fc72877e305b6193060144bef1ed2c1aeb76e
Edward Tanguay (windows) <edward@tanguay.info>
2023-06-25 09:09:14 +0200

    states works

--- src/examples/example_red_ball.ts
+++ src/examples/example_red_ball.ts
@@ -3,8 +3,31 @@ import { wrapAsExample } from '../components/wrapAsExample';
 
 export const example_red_ball = () => {
 	let html = '';
-	html += `
-		<div class="ball">the ball</div>	
-	`;
+
+	const states = [
+		"Baden-Württemberg",
+		"Bavaria",
+		"Berlin",
+		"Brandenburg",
+		"Bremen",
+		"Hamburg",
+		"Hesse",
+		"Lower Saxony",
+		"Mecklenburg-Vorpommern",
+		"North Rhine-Westphalia",
+		"Rhineland-Palatinate",
+		"Saarland",
+		"Saxony",
+		"Saxony-Anhalt",
+		"Schleswig-Holstein",
+		"Thuringia"
+	];
+
+	html += '<div class="states">';
+	states.forEach(state => {
+		html += `<div class="state">${state}</div>`;
+	})
+	html += '</div>'; 
+	
 	return wrapAsExample('example_red_ball', html);
 }
