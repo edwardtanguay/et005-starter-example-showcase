293e573cca3e6510c1b8507e31d162acfb9ad463
Edward Tanguay (windows) <edward@tanguay.info>
2023-06-25 11:43:29 +0200

    nouns out

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,10 @@
 import { example_blue_ball } from './examples/example_blue_ball';
-import { example_display_api_nouns } from './examples/example_display_api_nouns';
 import { example_german_states } from './examples/example_german_states';
 import './styles/main.scss';
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML =/* html */`
 <h1>Example Showcase</h1>
 <div class="examples">
-	${example_display_api_nouns()}
 	${example_blue_ball()}
 	${example_german_states()}
 </div>
