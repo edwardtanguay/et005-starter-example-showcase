eaa4713773d2ee887ee1da5c3a947995f1a59759
Edward Tanguay (windows) <edward@tanguay.info>
2023-06-25 12:05:40 +0200

    noun finished

--- src/examples/example_random_german_noun_flashcard.ts
+++ src/examples/example_random_german_noun_flashcard.ts
@@ -2,7 +2,7 @@ import './example_random_german_noun_flashcard.scss';
 import { wrapAsExample } from '../components/wrapAsExample';
 import nouns from '../data/germanNouns.json';
 
-const description =  '';
+const description = '';
 
 export const example_random_german_noun_flashcard = () => {
 	let html = '';
@@ -11,7 +11,10 @@ export const example_random_german_noun_flashcard = () => {
 	const noun = nouns[randomIndex];
 
 	html += `
-		<div class="ball">${noun.singular}</div>	
+	<div class="flashcard">
+		<div class="front">${noun.singular}</div>
+		<div class="back">${noun.article} ${noun.singular}</div>
+	</div>
 	`;
 	return wrapAsExample('example_random_german_noun_flashcard', html, description);
 }
