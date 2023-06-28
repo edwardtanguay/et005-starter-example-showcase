9106ca98b5e2cbdea5b20eb95ca1e12cd398b027
Edward Tanguay (windows) <edward@tanguay.info>
2023-06-28 21:52:23 +0200

    permalink shows highlighted

--- src/components/wrapAsExample.ts
+++ src/components/wrapAsExample.ts
@@ -3,14 +3,15 @@ import * as tools from '../tools';
 
 export const wrapAsExample = (idCode: string, html: string, description = '') => {
 	const html_description = description.trim() === '' ? '' : `<div class="description">${description}</div>`;
-
 	const codeUrl = `${config.githubRepositoryUrl}/blob/dev/src/examples/${idCode}.ts`;
 	const stylesUrl = `${config.githubRepositoryUrl}/blob/dev/src/examples/${idCode}.scss`;
 	const permalink = tools.createUrlCodeFromIdCode(idCode);
+	const currentPageIdCode = tools.getCurrentPageIdCode();
+
 
 	return /* html */ `
 	<fieldset class="example ${idCode}">
-		<legend>${idCode}</legend>
+		<legend class="${currentPageIdCode !== '' ? 'singleExample' : ''}">${idCode}</legend>
 		${html_description}
 		<div class="links">
 			<div><i class="fa fa-github" aria-hidden="true"></i> <a target="_blank" href=${codeUrl}>${idCode}.ts</a></div>
